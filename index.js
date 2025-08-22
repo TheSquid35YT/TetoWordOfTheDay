d184f44f6e611af3932f52651c0cc627b2915881
TheSquid35YT <39009874+TheSquid35YT@users.noreply.github.com>
2025-08-22 13:29:50 -0500

    Added more audio and end game conditions

--- index.js
+++ index.js
@@ -1,60 +1,47 @@
 'use strict';
 
-//const WORDS = window.words;
-
+//Initialize variables
+window.gameOver = false;
 const BACKSPACE_KEY = 'Backspace';
 const ENTER_KEY = 'Enter';
-//const WORD_LIST = window.words;
 const WORD_LIST = (window.words);
 const GUESS_LIST = (window.allowedWords);
 
-/*const WORD_LIST = [
-  'HELLO', 'PASTA', 'PANIC',
-  'SKILL', 'ARROW', 'BIRDS',
-  'FRUIT', 'PIZZA', 'SHAKE',
-  'THOSE', 'SUPER', 'SHARE',
-  'LOVES', 'WATER', 'VALUE',
-  'VALID', 'SWEET', 'START',
-  'SPACE', 'SLEEP', 'MAGIC',
-  'LEARN', 'LIGHT', 'LOGIC',
-  'HAPPY', 'HUMAN', 'GRAND',
-  'FOCUS', 'ENJOY', 'DREAM',
-  'CLEAN', 'CLEAR', 'WORDS',
-];*/
-
+//Choose word of the day
 const WORD_OF_THE_DAY = (WORD_LIST[getRandomIndex(WORD_LIST.length)]).toUpperCase();
+console.log(WORD_OF_THE_DAY);
 
-
-/*const WORD_OF_THE_DAY = (window.words)[Math.floor(Math.random() * (window.words).length)];
-console.log(window.words);*/
-
-// In case we want to make the game difficult or easier
+//Max attempts to guess the word
 const MAX_NUMBER_OF_ATTEMPTS = 6;
 
+//History of guesses
 const history = [];
 let currentWord = '';
 
-// Get everything setup and the game responding to user actions.
+//Initialize the game
 const init = () => {
-  console.log('👋 Welcome to Wordle');
+  //Welcome
+  console.log('Welcome to Teto Word of the Day!');
   playIntroAudio();
 
+  //Initialize keyboard keys
   const KEYBOARD_KEYS = ['QWERTYUIOP', 'ASDFGHJKL', 'ZXCVBNM'];
 
-  // Grab the gameboard and the keyboard
+  //Grab the gameboard and the keyboard
   const gameBoard = document.querySelector('#board');
   const keyboard = document.querySelector('#keyboard');
 
-  // Generate the gameboard and the keyboard
+  //Generate the gameboard and the keyboard
   generateBoard(gameBoard);
   generateBoard(keyboard, 3, 10, KEYBOARD_KEYS, true);
 
-  // Setup event listeners
+  //Create event listeners
   document.addEventListener('keydown', event => onKeyDown(event.key));
   gameBoard.addEventListener('animationend', event => event.target.setAttribute('data-animation', 'idle'));
   keyboard.addEventListener('click', onKeyboardButtonClick);
-}
+};
 
+//Show a given message in traditional Wordle style
 const showMessage = (message) => {
   const toast = document.createElement('li');
 
@@ -66,29 +53,29 @@ const showMessage = (message) => {
   setTimeout(() => toast.classList.add('fade'), 1000);
 
   toast.addEventListener('transitionend', (event) => event.target.remove());
-}
+};
 
+//Check the guess and assign attributes to each letter
 const checkGuess = (guess, word) => {
   const guessLetters = guess.split('');
   const wordLetters = word.split('');
   const remainingWordLetters = [];
   const remainingGuessLetters = [];
 
-  // Find the current active row
+  //Find the current active row
   const currentRow = document.querySelector(`#board ul[data-row='${history.length}']`);
 
-  // First, let's get all the columns in the current row set up with some base values
+  //First, let's get all the columns in the current row set up with some base values
   currentRow.querySelectorAll('li').forEach((element, index) => {
     element.setAttribute('data-status', 'none');
     element.setAttribute('data-animation', 'flip');
 
-    // Each letter should start its animation twice as late as the letter before it
+    //Each letter should start its animation twice as late as the letter before it
     element.style.animationDelay = `${index * 300}ms`;
     element.style.transitionDelay = `${index * 400}ms`;
   });
 
-  // Second iteration finds all the valid letters
-  // and creates a list of leftover letters
+  //Second iteration finds all the valid letters and creates a list of leftover letters
   wordLetters.forEach((letter, index) => {
     if (guessLetters[index] === letter) {
       currentRow.querySelector(`li:nth-child(${index + 1})`)
@@ -106,11 +93,12 @@ const checkGuess = (guess, word) => {
     }
   });
 
-  // Third iteration finds all the misplaced letters
+  //Third iteration finds all the misplaced letters
   remainingWordLetters.forEach(letter => {
-    // Skip this iteration, since the letter
-    // was already found in the previous phase
-    if (letter === false) return;
+    //If the letter was already found in the previous phase
+    if (letter === false) {
+      return;
+    };
 
     if (remainingGuessLetters.indexOf(letter) !== -1) {
       const column = currentRow
@@ -122,33 +110,48 @@ const checkGuess = (guess, word) => {
 
       if (keyboardKey.getAttribute('data-status') !== 'valid') {
         keyboardKey.setAttribute('data-status', 'invalid');
-      }
-    }
+      };
+    };
   });
 
-  // Fourth iteration finds all the letters on the keyboard
-  // that are absent from the word.
+  //Fourth iteration finds all the letters on the keyboard that are absent from the word.
   guessLetters.forEach(letter => {
     const keyboardKey = document.querySelector(`[data-key='${letter}']`);
     
     if (keyboardKey.getAttribute('data-status') === 'empty') {
       keyboardKey.setAttribute('data-status', 'none');
-    }
+    };
   });
 
+  //Update the history of guesses
   history.push(currentWord);
   currentWord = '';
-}
 
+  //Check if won
+  if ((history[history.length-1]) === (WORD_OF_THE_DAY)) {
+    victory();
+  } else {
+    //Don't allow more then 6 attempts to guess the word
+    if (history.length >= MAX_NUMBER_OF_ATTEMPTS) {
+      defeat();
+      return;
+    };
+  };
+};
+
+//Handle virtual keyboard button presses
 const onKeyboardButtonClick = (event) => {
-  if (event.target.nodeName === 'LI') {
+  if (event.target.nodeName === 'LI' && window.gameOver === false) {
     onKeyDown(event.target.getAttribute('data-key'));
-  }
-}
+  };
+};
 
+//Handle physical key presses
 const onKeyDown = (key) => {
-  // Don't allow more then 6 attempts to guess the word
-  if (history.length >= MAX_NUMBER_OF_ATTEMPTS) return;
+  //Don't allow more then 6 attempts to guess the word
+  if (history.length >= MAX_NUMBER_OF_ATTEMPTS || window.gameOver === true) {
+    return;
+  };
 
   // Find the current active row
   const currentRow = document.querySelector(`#board ul[data-row='${history.length}']`);
@@ -156,7 +159,7 @@ const onKeyDown = (key) => {
   // Find the next empty column in the current active row
   let targetColumn = currentRow.querySelector('[data-status="empty"]');
 
-  if (key === BACKSPACE_KEY) {
+  if (key === BACKSPACE_KEY && window.gameOver === false) {
     if (targetColumn === null) {
       // Get the last column of the current active row
       // as we are on the last column
@@ -177,7 +180,7 @@ const onKeyDown = (key) => {
     return;
   }
 
-  if (key === ENTER_KEY) {
+  if (key === ENTER_KEY && window.gameOver === false) {
     if (currentWord.length < 5) {
       playInvalidAudio();
       showMessage('Not enough letters');
@@ -268,14 +271,48 @@ function getRandomIndex (maxLength) {
 
 //Play sound for invalid word
 function playInvalidAudio() {
-  let invalidAudio = new Audio("Teto Huh.mp3");
+  let invalidAudio = new Audio("./Sounds/Invalid/Teto Huh.mp3");
   invalidAudio.volume = 0.25;
   invalidAudio.play();
 };
 
 //Play sound for intro
 function playIntroAudio() {
-  let introAudio = new Audio("Teto Word Of The Day Intro.mp3");
+  let introAudio = new Audio("./Sounds/Teto Word Of The Day Intro.mp3");
   introAudio.volume = 0.25;
   introAudio.play();
+};
+
+//Defeat
+function defeat() {
+  //Disable game functions
+  window.gameOver = true;
+
+  //Lose
+  setTimeout(function() {
+    showMessage(WORD_OF_THE_DAY);
+  }, 1700);
+};
+
+//Victory
+function victory() {
+  //Disable game functions
+  window.gameOver = true;
+  
+  //Choose random victory message
+  let randomWin = ((Math.floor(Math.random() * Math.floor(10)))+1)
+  
+  //Get sound
+  let winAudio = new Audio("./Sounds/Praise/Teto Praise "+randomWin+".mp3");
+  winAudio.volume = 0.75;
+  
+  //Get text
+  const winMessages = ["Good job!", "Well done!", "Nice work!", "You\'re awesome!", "Great effort!", "You did it!", "That\'s amazing!", "I\'m proud of you!", "Keep it up!", "You\'re doing great"];
+  let winMessage = winMessages[randomWin-1];
+  
+  //Win
+  setTimeout(function() {
+    winAudio.play();
+    showMessage(winMessage);
+  }, 1650);
 };
